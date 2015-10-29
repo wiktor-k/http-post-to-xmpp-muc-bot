aab10acf951b7ea28ad3a909c7330d89d8817f92
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2015-10-29 13:54:02 +0100

    Add support for for-each (map)

--- nanotemplates.py
+++ nanotemplates.py
@@ -1,19 +1,42 @@
 import unittest
 import json
 import re
+import sys
 
 def render(data, template):
   def get_key(obj, key):
     return None if obj is None else obj.get(key)
 
+  def replaceMap(match):
+    container = get_key(data, match.group(1))
+    subtemplate = match.group(2)
+    def in_scope(obj):
+      if not isinstance(obj, dict):
+        return render(obj, subtemplate)
+      obj[''] = data.get('')
+      data[''] = obj
+      text = render(data, subtemplate)
+      data[''] = obj['']
+      return text
+    return ''.join(map(in_scope, container))
+
+  # replace maps
+  text = re.sub(r'{map ([^ /}]+)}(.*?){/map}', replaceMap, template)
+  
+  # replace access to properties
   def replace(match):
     return reduce(get_key, match.group(1).split('.'), data)
 
-  return re.sub(r'{([^}]+)}', replace, template)
+  text = re.sub(r'{([^/][^ }]+)}', replace, text)
+  
+  # replace objects themselves
+  text = text.replace('{}', str(data))
+  
+  return text
 
 # tests
 
-fixture = json.loads("""{"name":"project","url":"job/project/","build":{"full_url":"http://jenkins/project/15/","number":15,"phase":"FINALIZED","status":"SUCCESS","url":"job/sap-migrator/15/","scm":{"url":"git","branch":"origin/master","commit":"abc"},"log":"[...truncated 206 lines...]\\nFinished Calculation of disk usage of workspace in 0 seconds\\nFinished: SUCCESS\\n","artifacts":{"project.jar":{"archive":"http://jenkins/project"}}}}""")
+fixture = json.loads("""{"name":"project","url":"job/project/","build":{"full_url":"http://jenkins/project/15/","number":15,"phase":"FINALIZED","status":"SUCCESS","url":"job/project/15/","scm":{"url":"git","branch":"origin/master","commit":"abc"},"log":"[...truncated 206 lines...]\\nFinished Calculation of disk usage of workspace in 0 seconds\\nFinished: SUCCESS\\n","artifacts":{"project.jar":{"archive":"http://jenkins/project"}}},"commits":[{"name":"one"},{"name":"two"},{"name":"three"}],"strings":["a","b","c"]}""")
 
 class TestStringMethods(unittest.TestCase):
 
@@ -27,3 +50,11 @@ Finished: SUCCESS
 
   def test_no_key(self):
     self.assertEqual(render(fixture, "a{does.not.exist}b"), "ab")
+
+  def test_map(self):
+    self.assertEqual(render(fixture, "a-{map commits}{.name}-{/map}"), "a-one-two-three-")
+    self.assertEqual(render(fixture, "a-{map commits}{.name}-{/map}{name}"), "a-one-two-three-project")
+    self.assertEqual(render(fixture, "a-{map strings}{}-{/map}{name}"), "a-a-b-c-project")
+
+if __name__ == "__main__":
+  print render(json.loads('\n'.join(sys.stdin.readlines())), sys.argv[1])
